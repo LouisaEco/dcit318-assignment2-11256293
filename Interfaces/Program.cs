0de62780f287afd2786e51e739a22bad5013e1d6
Kornu <lxekornu@st.ug.edu.gh>
2025-08-06 17:27:36 +0000

    Adding interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Define interface
+interface IMovable
+{
+    void Move();
+}
+
+// Class: Car
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class: Bicycle
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class InterfaceDemo
+{
+    static void Main(string[] args)
+    {
+        IMovable car = new Car();
+        IMovable bike = new Bicycle();
+
+        car.Move();   // Outputs: Car is moving
+        bike.Move();  // Outputs: Bicycle is moving
+    }
+}
